9ceac197a4a95df01b8aaba502c63de632ca95e7
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-10-26 17:20:26 +0100

    split out api, getting and receiving data classes

--- lib/get_data.rb
+++ lib/get_data.rb
@@ -1,18 +1,80 @@
 require 'JSON'
 require 'net/http'
 
-users = []
-hash_key = 'data'
 
-i = 0
-until i*1000 > users.count
-  url = "https://driftrock-dev-test-2.herokuapp.com/users?per_page=1000&page=#{i}"
-  uri = URI(url)
-  response = Net::HTTP.get(uri)
-  userdata = JSON.parse(response)[hash_key]
-  users += userdata
-  i += 1
+class GetUserData
+  attr_reader :users
+
+  def initialize(api = APICall)
+    @users = []
+    @users_end = "users"
+    @api = api
+  end
+
+  def user_data
+    GetData.get_all_data(@users_end, @users)
+  end
+
 end
 
-puts users.count
-puts i
+class GetPurchasesData
+  attr_reader :purchases
+
+  def initialize(api = APICall)
+    @purchases = []
+    @purchases_end = "purchases"
+    @api = api
+  end
+
+end
+
+
+
+
+class GetData
+
+
+
+
+  def add_to_array(local_array, parsed_data)
+    local_array += parsed_data
+  end
+
+  def no_more_records?(page, records, received)
+    page*records > received.count
+  end
+
+  def get_all_data(destination, local_array, api)
+    local_array = []
+    page = 0
+    records = 1000
+    until no_more_records?(page, records, local_array)
+      parsed_data = api.get_and_parse(destination, page, records)
+      add_to_array(local_array, parsed_data)
+      page += 1
+    end
+  end
+
+end
+
+
+
+class APICall
+
+  @hash_key = 'data'
+
+  def get_json(uri)
+    Net::HTTP.get(uri)
+  end
+
+  def set_uri(destination, page, records)
+    URI("https://driftrock-dev-test-2.herokuapp.com/#{destination}?per_page=#{records}&page=#{page}")
+  end
+
+  def self.get_and_parse(destination, page, records)
+    json_data = get_json(set_uri(destination, page, records))
+    JSON.parse(json_data)[@hash_key]
+  end
+
+
+end
---
+++ spec/get_data_spec.rb
@@ -0,0 +1,9 @@
+require 'get_data'
+
+describe APICall do
+    let :subject {described_class}
+
+
+    it 'can return user id from email' do
+      expect(subject.get_data).to eq "12DF-2324-GA2D-31RT"
+    end
